01ea111b039652b738fff29b4b0d4def8d984b09
horizonltd <learntoprogress@yahoo.com>
2020-03-31 02:19:01 +0100

    debug

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2y3*%r@d)w5+9@*)i3tdar5q)=^rl^o@zii7)3m4#l^xmr&cqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True)
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'coronang.herokuapp.com']
 
